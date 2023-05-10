236b59aedf23a2bdf1706e3350441afae2cdf1fb
pin <a0968155661@gmail.com>
2023-05-10 21:27:35 +0800

    JS 基礎

---
+++ dist/js/js_base.js
@@ -0,0 +1,8 @@
+let age = 18;
+let firstName = 'PINSIOU';
+let lastName = 'GUO';
+let country = 'TW';
+
+console.log('(' + country + ') ' + firstName + ' ' + lastName);
+
+console.log(`(${country}) ${firstName} ${lastName}`);
