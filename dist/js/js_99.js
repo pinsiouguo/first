4cc96a669e1a70e56fa1476127d74d530eecc214
pin <a0968155661@gmail.com>
2023-05-17 20:48:55 +0800

    if是否可執行

--- dist/js/js_99.js
+++ dist/js/js_99.js
@@ -1,5 +1,24 @@
 const genTable = () => {
-    console.log('123');
+    let n1 = document.querySelector('#n1');
+    let n2 = document.querySelector('#n2');
+
+    if (!n1) {
+        return;
+    }
+
+    if (n1.value <= 0) {
+        return;
+    }
+
+    if (!n2) {
+        return;
+    }
+
+    if (n2.value <= 0) {
+        return;
+    }
+
+    console.log('I can!');
 }
 
 
