352b0d9fa0b293e0fa3833d5f5b870000322a8a1
pin <a0968155661@gmail.com>
2023-05-22 21:25:12 +0800

    實作手機選單 icon 切換

--- dist/js/js_dom_control.js
+++ dist/js/js_dom_control.js
@@ -7,3 +7,26 @@ doing.addEventListener('click', () => {
     uid.style.fontSize = '100px';
     uid.style.backgroundColor = 'yellow';
 });
+
+let addClass = document.querySelector('#add-class');
+
+addClass.addEventListener('click', () => {
+    uid.classList.add('active');
+});
+
+let removeClass = document.querySelector('#remove-class');
+
+removeClass.addEventListener('click', () => {
+    uid.classList.remove('active');
+});
+
+let mobileIcon = document.querySelector('#mobile-icon');
+
+
+mobileIcon.addEventListener('click', () => {
+    if (mobileIcon.classList.contains('active')) {
+        mobileIcon.classList.remove('active');
+    } else {
+        mobileIcon.classList.add('active');
+    }
+});
