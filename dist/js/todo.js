f456e252fe4c330f452aae026e4419ea639fdee1
pin <a0968155661@gmail.com>
2023-05-31 20:40:42 +0800

    新增項目實作

--- dist/js/todo.js
+++ dist/js/todo.js
@@ -19,7 +19,26 @@ const init = () => {
             return;
         }
 
-        console.log(value);
+        let ul = document.querySelector('#pending-item');
+
+        // 正規作法
+        // let li = document.createElement('li');
+        // let checkbox = document.createElement('input');
+        // let label = document.createElement('label');
+        // checkbox.type = 'checkbox';
+        // label.innerHTML = value;
+        // li.appendChild(checkbox);
+        // li.appendChild(label);
+
+        // ul.appendChild(li);
+
+        // 簡單作法
+        ul.innerHTML += `
+        <li>
+            <input type="checkbox">
+            <label for="">${value}</label>
+        </li>
+        `;
     }
 
     addBtn.addEventListener('click', () => {
