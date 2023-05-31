3d901f3455e700ee45bf333b628390dc95d766bf
pin <a0968155661@gmail.com>
2023-05-31 20:13:16 +0800

    使用第三方套件取代 alert

--- dist/js/todo.js
+++ dist/js/todo.js
@@ -2,31 +2,34 @@ const init = () => {
     let addBtn = document.querySelector('#add-btn');
     let itemName = document.querySelector('#item-name');
 
-    addBtn.addEventListener('click', () => {
+    // 新增項目統一收納
+    const doAddItem = () => {
         let value = itemName.value;
 
         if (!value) {
-            alert('請輸入項目名稱');
+            Swal.fire({
+                title: '新增錯誤',
+                text: '請輸入項目名稱',
+                icon: 'error',
+            })
             itemName.focus();
             return;
         }
 
         console.log(value);
+    }
+
+    addBtn.addEventListener('click', () => {
+        doAddItem();
     });
 
     itemName.addEventListener('keyup', (e) => {
         if (e.key == 'Enter') {
-            let value = itemName.value;
-
-            if (!value) {
-                alert('請輸入項目名稱');
-                itemName.focus();
-                return;
-            }
-
-            console.log(value);
+            doAddItem();
         }
     });
+
+
 };
 
 init();
