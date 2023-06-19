449f055ffca4a81530ff50d05502d93eb7095800
pin <a0968155661@gmail.com>
2023-06-19 21:10:19 +0800

    背景圖片載入 選單 active 樣式附加 點選選單異動 currentIndex

--- dist/js/final_layout.js
+++ dist/js/final_layout.js
@@ -5,7 +5,13 @@ Vue.createApp({
                 { title: '第一張', subtitle: '第一子標題', bg: '/images/dog.jpeg' },
                 { title: '第二張', subtitle: '第二子標題', bg: '/images/banner.jpeg' },
                 { title: '第三張', subtitle: '第三子標題', bg: '/images/process.jpeg' },
-            ]
+            ],
+            currentIndex: 0,
+        }
+    },
+    methods: {
+        setIndex(index) {
+            this.currentIndex = index;
         }
     }
 }).mount('#app-slider');
